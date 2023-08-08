10ef4fb9d533085d523caa7a8b009bb40a0077af
kimhr <khl9566@naver.com>
2023-08-08 23:57:47 +0900

    commit

---
+++ Test1/ToDoListViewController.swift
@@ -0,0 +1,149 @@
+//
+//  ToDoListViewController.swift
+//  Test1
+//
+//  Created by t2023-m0067 on 2023/08/07.
+//
+
+import UIKit
+
+// 테이블 셀 등록 설정
+protocol UITableViewRegisterable {
+    static var cellId: String { get }
+    static var isFromNib: Bool { get }
+    
+    static func register(target: UITableView)
+}
+
+extension UITableViewRegisterable where Self: UITableViewCell {
+    static func register(target: UITableView){
+        if self.isFromNib {
+            target.register(UINib(nibName: self.cellId, bundle: nil), forCellReuseIdentifier: self.cellId)
+        } else {
+            target.register(Self.self, forCellReuseIdentifier: self.cellId)
+        }
+    }
+}
+
+class CustomCell:UITableViewCell, UITableViewRegisterable {
+    static var cellId: String {
+        get {
+            return "MyTableViewCell"
+        }
+    }
+    static var isFromNib: Bool {
+        get {
+            return false
+        }
+    }
+
+}
+
+// 등록 데이터 다른 클래스 생성해서 거기서 데이터 받아오기 싱글톤패턴
+struct Memo{
+    var title: String?
+    var isCompleted: Bool = false
+}
+
+class MemoStore {
+    static var data: Array<Memo> = []
+}
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    // 테이블 뷰
+    var tableView: UITableView!
+    // 스위치 호출
+    var onOff: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // 테이블 뷰 초기화
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // 등록한 셀 호출
+        CustomCell.register(target: self.tableView)
+
+        // 테이블 뷰 등록
+        view.addSubview(tableView)
+        
+    }
+
+    // 등록 처리
+    @IBAction func BarButtonAction(_ sender: Any) {
+
+        let alert = UIAlertController(title: "등록", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField {(textField) in
+            textField.placeholder = "할 일을 입력하세요"
+        }
+        
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "추가", style: .default, handler: { action in
+            if let toDo = alert.textFields?.first?.text{
+                MemoStore.data.append(Memo(title: toDo))
+                self.tableView.reloadData()
+            }
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // 테이블 뷰 셀 개수 반환
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MemoStore.data.count
+    }
+    
+
+    // 테이블 뷰 셀 설정
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "MyTableViewCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableViewCell", for: indexPath)
+        
+        // 스위치 호출
+        onOff = UISwitch(frame: .zero)
+        // switch tag 지정
+        onOff.tag = indexPath.row
+        // 초기 생성 시 버튼 누르지 않은 채로 생성
+        onOff.isOn = MemoStore.data[indexPath.row].isCompleted
+        // switch addtarget 지정
+        onOff.addTarget(self, action: #selector(self.switchDidChange(_:)), for: .valueChanged)
+        //cell accessoryView를 switch로 지정
+        cell.accessoryView = onOff
+
+        // 테이블뷰 생성
+        cell.textLabel?.text = MemoStore.data[indexPath.row].title
+        
+        return cell
+    }
+
+    // 스위치 상태 바뀔 때 마다 처리
+    @objc func switchDidChange(_ sender: UISwitch){
+       
+        if sender.isOn == true {
+            MemoStore.data[sender.tag].isCompleted = true
+        }else{
+            MemoStore.data[sender.tag].isCompleted = false
+        }
+        
+    }
+    
+    // 테이블 뷰 셀 선택 처리
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 선택된 셀의 항목 출력
+        print("선택된 항목: \(MemoStore.data[indexPath.row])")
+    }
+    
+}
+
+
+
+
+
+
---
+++ Test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by t2023-m0067 on 2023/08/04.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
